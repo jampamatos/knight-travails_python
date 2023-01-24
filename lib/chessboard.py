18c91c759765883c33efbe6afcf8289da8beba2e
João Paulo <jp.coutm@gmail.com>
2023-01-24 18:38:32 -0300

    Created Venv for project

--- lib/chessboard.py
+++ lib/chessboard.py
@@ -1,9 +1,15 @@
+from square import Square
+
 class ChessBoard:
-  size = None
-  def __init__(self):
-    self.set_size(8)
-    self.board = [[0 for _ in range(self.size)] for _ in range(self.size)]
-  
-  @classmethod
-  def set_size(cls, size):
-    cls.size = size
+    size = None
+
+    def __init__(self):
+        self.set_size(8)
+        self.create_squares()
+
+    def create_squares(self):
+        self.board = [[Square(x, y) for x in range(self.size)] for y in range(self.size)]
+
+    @classmethod
+    def set_size(cls, size):
+        cls.size = size
